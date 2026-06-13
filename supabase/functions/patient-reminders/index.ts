00555e6536c4550bbe0c326bf349aa629cc5e4d9
Eric Obulo <obulo.eric@gmail.com>
2026-06-13 07:44:15 +0100

    Update index.ts

--- supabase/functions/patient-reminders/index.ts
+++ supabase/functions/patient-reminders/index.ts
@@ -2,7 +2,7 @@ import { createClient } from "npm:@supabase/supabase-js@2";
 
 const supabase = createClient(
   Deno.env.get("SUPABASE_URL")!,
-  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
+  Deno.env.get("SERVICE_KEY")!
 );
 
 Deno.cron("Patient Reminders", "0 6,14 * * *", async () => {
